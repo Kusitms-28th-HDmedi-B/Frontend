11308891471ee7736a6c0bbcf2132fa5575f99bf
j2noo <wlsdndml213@naver.com>
2023-09-04 22:02:13 +0900

    chore(#12) : airbnb eslint, prettier 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,31 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-refresh'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
   },
+  '@typescript-eslint/interface-name-prefix': 'on',
+  '@typescript-eslint/explicit-function-return-type': 'on',
+  '@typescript-eslint/explicit-module-boundary-types': 'on',
+  '@typescript-eslint/no-explicit-any': 'on',
 };
--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 
 function App() {
+  const a = 1;
   return <>test</>;
 }
 
