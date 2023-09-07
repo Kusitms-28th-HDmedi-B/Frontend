abaa8e06b5361815ea6af24b46fc94d797bf2312
j2noo <wlsdndml213@naver.com>
2023-09-07 23:48:30 +0900

    refactor(#18) : 네이버지도 코드 개선

--- src/pages/info/index.tsx
+++ src/pages/info/index.tsx
@@ -13,44 +13,48 @@ const Title = styled.div`
   align-items: center;
 `;
 
+const Map = styled.div`
+  width: 400px;
+  height: 400px;
+`;
+
 const TextBox = styled.div`
   width: 100px;
   height: 100px;
 `;
+
+// window 타입 정의 추가
+declare global {
+  interface Window {
+    naver: any;
+  }
+}
+
+const initMap = () => {
+  const map = new window.naver.maps.Map('map', {
+    //지도 추가, 좌표를 기점으로 주변 지도가 추가된다.
+    center: new window.naver.maps.LatLng(37.56981765163963, 126.97805713023197),
+    zoom: 15,
+  });
+
+  const marker = new window.naver.maps.Marker({
+    position: new window.naver.maps.LatLng(
+      37.56981765163963,
+      126.97805713023197,
+    ),
+    map: map,
+  });
+};
+
 const Info = () => {
   useEffect(() => {
-    let map = null;
-    let marker = null;
-    const initMap = () => {
-      map = new window.naver.maps.Map('map', {
-        //지도 추가, 좌표를 기점으로 주변 지도가 추가된다.
-        center: new window.naver.maps.LatLng(
-          37.56981765163963,
-          126.97805713023197,
-        ),
-        zoom: 15,
-      });
-
-      marker = new window.naver.maps.Marker({
-        position: new window.naver.maps.LatLng(
-          37.56981765163963,
-          126.97805713023197,
-        ), //Marker 추가, 좌표에 마커가 찍힌다.
-        map: map,
-      });
-    };
-    initMap();
+    initMap(); // 네이버지도는 1회만!
   }, []);
 
-  const mapStyle = {
-    width: '45vw',
-    height: '22vw',
-  };
   return (
     <Container>
       <Title>위치</Title>
-      {/* <Map></Map> */}
-      <div id="map" style={mapStyle} />
+      <Map id="map"></Map>
       <TextBox></TextBox>
     </Container>
   );
