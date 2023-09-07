e0cd9ea7d1b4e73ff18a3409260ff1133c0bba8d
j2noo <wlsdndml213@naver.com>
2023-09-07 23:33:33 +0900

    feat(#18) : 네이버지도 api 불러오기 성공

--- src/pages/info/index.tsx
+++ src/pages/info/index.tsx
@@ -1,5 +1,59 @@
+import { styled } from 'styled-components';
+import { useEffect } from 'react';
+
+const Container = styled.div`
+  background-color: white;
+  width: 80%;
+  margin: auto;
+`;
+const Title = styled.div`
+  font-size: 36px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TextBox = styled.div`
+  width: 100px;
+  height: 100px;
+`;
 const Info = () => {
-  return <div>Info</div>;
+  useEffect(() => {
+    let map = null;
+    let marker = null;
+    const initMap = () => {
+      map = new window.naver.maps.Map('map', {
+        //지도 추가, 좌표를 기점으로 주변 지도가 추가된다.
+        center: new window.naver.maps.LatLng(
+          37.56981765163963,
+          126.97805713023197,
+        ),
+        zoom: 15,
+      });
+
+      marker = new window.naver.maps.Marker({
+        position: new window.naver.maps.LatLng(
+          37.56981765163963,
+          126.97805713023197,
+        ), //Marker 추가, 좌표에 마커가 찍힌다.
+        map: map,
+      });
+    };
+    initMap();
+  }, []);
+
+  const mapStyle = {
+    width: '45vw',
+    height: '22vw',
+  };
+  return (
+    <Container>
+      <Title>위치</Title>
+      {/* <Map></Map> */}
+      <div id="map" style={mapStyle} />
+      <TextBox></TextBox>
+    </Container>
+  );
 };
 
 export default Info;
