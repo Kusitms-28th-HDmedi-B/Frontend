36b9bba70b4629968d01f61044e0feb6597efc93
j2noo <wlsdndml213@naver.com>
2023-09-07 23:55:37 +0900

    feat(#18) : map 컴포넌트 분리

---
+++ src/pages/info/Map.tsx
@@ -0,0 +1,40 @@
+import { styled } from 'styled-components';
+import { useEffect } from 'react';
+
+const MapDiv = styled.div`
+  width: 700px;
+  height: 300px;
+`;
+
+// window 타입 정의 추가
+declare global {
+  interface Window {
+    naver: any;
+  }
+}
+
+const initMap = () => {
+  const map = new window.naver.maps.Map('map', {
+    //지도 추가, 좌표를 기점으로 주변 지도가 추가된다.
+    center: new window.naver.maps.LatLng(37.56981765163963, 126.97805713023197),
+    zoom: 15,
+  });
+
+  const marker = new window.naver.maps.Marker({
+    position: new window.naver.maps.LatLng(
+      37.56981765163963,
+      126.97805713023197,
+    ),
+    map: map,
+  });
+};
+
+const Map = () => {
+  useEffect(() => {
+    initMap(); // 네이버지도는 1회만!
+  }, []);
+
+  return <MapDiv id="map"></MapDiv>;
+};
+
+export default Map;
--- src/pages/info/index.tsx
+++ src/pages/info/index.tsx
@@ -1,5 +1,5 @@
 import { styled } from 'styled-components';
-import { useEffect } from 'react';
+import Map from './Map';
 
 const Container = styled.div`
   background-color: white;
@@ -13,48 +13,16 @@ const Title = styled.div`
   align-items: center;
 `;
 
-const Map = styled.div`
-  width: 400px;
-  height: 400px;
-`;
-
 const TextBox = styled.div`
   width: 100px;
   height: 100px;
 `;
 
-// window 타입 정의 추가
-declare global {
-  interface Window {
-    naver: any;
-  }
-}
-
-const initMap = () => {
-  const map = new window.naver.maps.Map('map', {
-    //지도 추가, 좌표를 기점으로 주변 지도가 추가된다.
-    center: new window.naver.maps.LatLng(37.56981765163963, 126.97805713023197),
-    zoom: 15,
-  });
-
-  const marker = new window.naver.maps.Marker({
-    position: new window.naver.maps.LatLng(
-      37.56981765163963,
-      126.97805713023197,
-    ),
-    map: map,
-  });
-};
-
 const Info = () => {
-  useEffect(() => {
-    initMap(); // 네이버지도는 1회만!
-  }, []);
-
   return (
     <Container>
       <Title>위치</Title>
-      <Map id="map"></Map>
+      <Map />
       <TextBox></TextBox>
     </Container>
   );
