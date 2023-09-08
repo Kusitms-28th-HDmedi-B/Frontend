904d1865323561ef2484261f3c672644611f2849
j2noo <wlsdndml213@naver.com>
2023-09-08 18:08:44 +0900

    feat(#26) pageBar 적용

--- src/pages/recruit/index.tsx
+++ src/pages/recruit/index.tsx
@@ -3,6 +3,7 @@ import { Title } from '../info';
 import CheckBox from './CheckBox';
 import { useState } from 'react';
 import arrow from '../../assets/arrow.png';
+import PageBar from '../../components/common/PageBar';
 
 const ContentsContainer = styled.div`
   display: flex;
@@ -27,12 +28,18 @@ const ReCruitsList = styled.li`
 `;
 const Recruit = () => {
   const datas = [
-    '[HR] [신입/경력] 2023 전국 대규모 공개채용 초봉 1억 시작 내집마련의 꿈 내집마련의 꿈v 내집마련의 꿈  ',
-    '[IT] SW엔지니어 모집 공고 당신도 이재용이 될 수 있습니다',
-    '[마케팅] 마케팅 전문가 제2의 스티브잡스 모집 공고',
-    '[회계] 니 돈을 내 돈처럼 사용하라, 회삿돈으로 비트코인 가능',
+    '1[HR] [신입/경력] 2023 전국 대규모 공개채용 초봉 1억 시작 내집마련의 꿈 내집마련의 꿈v 내집마련의 꿈  ',
+    '2[IT] SW엔지니어 모집 공고 당신도 이재용이 될 수 있습니다',
+    '3[마케팅] 마케팅 전문가 제2의 스티브잡스 모집 공고',
+    '4[회계] 니 돈을 내 돈처럼 사용하라, 회삿돈으로 비트코인 가능',
+    '5[HR] [신입/경력] 2023 전국 대규모 공개채용 초봉 1억 시작 내집마련의 꿈 내집마련의 꿈v 내집마련의 꿈  ',
+    '6[IT] SW엔지니어 모집 공고 당신도 이재용이 될 수 있습니다',
+    '7[마케팅] 마케팅 전문가 제2의 스티브잡스 모집 공고',
+    '8[회계] 니 돈을 내 돈처럼 사용하라, 회삿돈으로 비트코인 가능',
   ];
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
+  // const pageBarProps = { maxPage: 2, page: 0, setPage: setPage };c
+  console.log('rerendeR1');
   return (
     <>
       <Title>채용 공고</Title>
@@ -40,7 +47,7 @@ const Recruit = () => {
         <CheckBox />
         <RecruitsContainer>
           <ul>
-            {datas.slice(page * 3, page * 3 + 3).map((data) => (
+            {datas.slice((page - 1) * 3, (page - 1) * 3 + 3).map((data) => (
               <>
                 <ReCruitsList>
                   {data}{' '}
@@ -53,6 +60,7 @@ const Recruit = () => {
               </>
             ))}
           </ul>
+          <PageBar maxPage={3} page={page} setPage={setPage}></PageBar>
         </RecruitsContainer>
       </ContentsContainer>
     </>
