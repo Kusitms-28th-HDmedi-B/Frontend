1208a499b84fd9d94332dc6eb0574a25b5c4eb49
j2noo <wlsdndml213@naver.com>
2023-09-09 00:56:48 +0900

    feat(#38) 공지사항 컴포넌트 완료

--- src/pages/notice/index.tsx
+++ src/pages/notice/index.tsx
@@ -1,5 +1,20 @@
+import styled from 'styled-components';
+import { Title } from '../info';
+import Announcement from './Announce';
+
+const Container = styled.div`
+  //공통적용, 리팩터링
+  width: 70%;
+  margin: auto;
+  margin-bottom: 100px;
+`;
+
 const Notice = () => {
-  return <div>Notice</div>;
+  return (
+    <Container>
+      <Announcement></Announcement>
+    </Container>
+  );
 };
 
 export default Notice;
