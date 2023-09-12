1e82b88a7e17ee41f3a27823bc996b2c900bfa49
j2noo <wlsdndml213@naver.com>
2023-09-12 17:40:02 +0900

    feat(#60) : 네비게이션 바 애니메이션 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -25,27 +25,32 @@ const Nav = styled.ul`
   margin-left: 100px;
   display: inline-block;
   position: absolute;
-
-  li {
-    position: relative;
-    display: inline;
-    margin: 0 30px;
-    padding-bottom: 15px; //호버시 마우스를 내려올 수 있게 하기 위한 영역
-    text-align: center;
-    line-height: 28px; //이거다
-    color: #8b8b8b;
-    &:hover {
-      font-weight: 700;
-      color: #31313e;
-    }
+`;
+const NavLi = styled.li<{ countDepth: number }>`
+  position: relative;
+  display: inline;
+  padding: 0px 30px 15px 30px;
+  /* margin: 0 30px;
+  padding-bottom: 15px; //호버시 마우스를 내려올 수 있게 하기 위한 영역 */
+  text-align: center;
+  line-height: 28px; //이거다
+  color: #8b8b8b;
+  &:hover {
+    font-weight: 700;
+    color: #31313e;
   }
-  li:hover > div {
-    display: flex; /* 부모 li에 호버 시 나타남 */
-    flex-direction: column;
+  &:hover > div {
+    height: ${(props) => props.countDepth * 34 + 3}px;
+    border: 1px solid #d0d0d0;
   }
 `;
 const NavDepthContainer = styled.div`
-  display: none;
+  display: flex; /* 부모 li에 호버 시 나타남 */
+  flex-direction: column;
+  height: 0px;
+  overflow: hidden;
+  transition: height 0.3s ease-in-out;
+
   position: absolute;
   width: max-content;
 
@@ -53,7 +58,6 @@ const NavDepthContainer = styled.div`
   /* left: 0; //부모의 li가 relative이므로, 그것의 x값 */
   left: 50%; /* 요소의 가로 중앙으로 이동 */
   transform: translateX(-50%); /* 왼쪽으로 절반만큼 이동 */
-  border: 1px solid #d0d0d0;
   border-radius: 9px;
   /* box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2); */
   font-size: 14px;
@@ -82,13 +86,14 @@ type LanguageType = 'KR' | 'EN';
 function Header() {
   const categories = CategoryData;
   const [currLanguage, setCurrLanguage] = useState<LanguageType>('KR');
+  const [isHover, setIsHover] = useState(false);
   console.log('rende');
   return (
     <Container>
       <Logo></Logo>
       <Nav>
         {categories.map((category, index) => (
-          <li key={index}>
+          <NavLi key={index} countDepth={category.depth.length}>
             {category.title}
             <NavDepthContainer>
               {category.depth.map((each, index) => (
@@ -97,7 +102,7 @@ function Header() {
                 </NavDepth>
               ))}
             </NavDepthContainer>
-          </li>
+          </NavLi>
         ))}
       </Nav>
       <LanguageContainer>
