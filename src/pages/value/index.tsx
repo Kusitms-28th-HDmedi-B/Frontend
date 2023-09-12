7e5bc6bdd3cff84ea4026a76074b23ce435beb41
j2noo <wlsdndml213@naver.com>
2023-09-12 23:46:03 +0900

    feat(#55) : 배너 완성

--- src/pages/value/index.tsx
+++ src/pages/value/index.tsx
@@ -1,5 +1,33 @@
+import styled from 'styled-components';
+import { useSetRecoilState } from 'recoil';
+import { currHeaderAtom } from '../../recoil/atom';
+import { useEffect } from 'react';
+
+import banner from '../../../public/assets/image/ValueBannerEx.png';
+import { bannerText } from './valueData';
+
+const Container = styled.div`
+  width: 100vw;
+  margin-bottom: 100px;
+`;
+const Banner = styled.div`
+  width: 100%;
+  height: 700px;
+  background: url(${banner}) no-repeat center/cover;
+
+  text-align: center;
+  line-height: 700px;
+  font-size: 50px;
+  font-weight: 700;
+`;
 const Value = () => {
-  return <div>Value</div>;
+  const setCurrHeader = useSetRecoilState(currHeaderAtom);
+  useEffect(() => setCurrHeader('transparent'));
+  return (
+    <Container>
+      <Banner>{bannerText}</Banner>
+    </Container>
+  );
 };
 
 export default Value;
---
+++ src/pages/value/valueData.ts
@@ -0,0 +1,1 @@
+export const bannerText = '사회의 문제를 함게 고민할 수 있는 인재를 찾습니다!';
