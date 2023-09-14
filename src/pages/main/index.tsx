e302cff340adffbcb8afb062e41900131a2b022b
j2noo <wlsdndml213@naver.com>
2023-09-14 22:01:09 +0900

    feat(#69) : 뉴스 컨테이너 이전까지 완성

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -7,36 +7,66 @@ import Banner from './Banner';
 const Container = styled.div`
   width: 100vw;
 `;
+const SecondContainer = styled.div`
+  width: 70%;
+  margin: auto;
+  height: 100vw; //없애?
+`;
+const SecondText = styled.div`
+  margin: 150px 0;
+  white-space: break-spaces;
+  font-size: 30px;
+  font-weight: 600;
+  line-height: 40px;
+  text-align: center;
+  :nth-child(2) {
+    color: #7aa4e8;
+  }
+`;
+const SecondGif = styled.div`
+  margin: auto;
+  width: 600px;
+  height: 600px;
+  background-color: red;
+`;
+const ThirdContainer = styled.div`
+  width: 70%;
+  margin: auto;
+`;
+const ThirdTitle = styled.div`
+  font-size: 30px;
+  font-weight: 600;
+  white-space: break-spaces;
+`;
+const FourthContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #1a1f27;
+`;
 const Main = () => {
   const setCurrHeader = useSetRecoilState(currHeaderAtom);
   useEffect(() => setCurrHeader('transparent'));
-
   return (
     <Container>
       <Banner></Banner>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
-      <div>Maasdasdin</div>
+      <SecondContainer>
+        <SecondText>
+          <span>{'에이치디메디는\n'}</span>
+          <span>{'지역 약사와 지역 환자를 연결'}</span>
+          <span>
+            {
+              '하는 복약지도를 제공해\n의약품의 부작용을 줄이고, 더 건강한 사회를 약속드립니다.'
+            }
+          </span>
+        </SecondText>
+        <SecondGif>s</SecondGif>
+      </SecondContainer>
+      <ThirdContainer>
+        <ThirdTitle>
+          {"에이치디메디는\n'지속가능한 사회'를 추구합니다."}
+        </ThirdTitle>
+      </ThirdContainer>
+      <FourthContainer>복약지도4만건건건</FourthContainer>
     </Container>
   );
 };
