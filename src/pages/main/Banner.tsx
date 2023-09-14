75cc7bfdd9c4fef480a59896f234181d22ad3c5f
j2noo <wlsdndml213@naver.com>
2023-09-14 18:14:17 +0900

    chore(#69) : swiperjs 적용

--- src/pages/main/Banner.tsx
+++ src/pages/main/Banner.tsx
@@ -1,18 +1,59 @@
-import styled from 'styled-components';
-import img1 from '../../../public/assets/image/HomeBanner2.png';
-// import img2 from '../../../public/assets/image/HomeBanner2.png'
-// import img3 from '../../../public/assets/image/HomeBanner3.png'
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-const Container = styled.div`
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import styled from 'styled-components';
+
+import img1 from '../../../public/assets/image/HomeBanner2.png';
+import img2 from '../../../public/assets/image/HomeBanner2.png';
+import img3 from '../../../public/assets/image/HomeBanner3.png';
+
+const SwiperContainer = styled(Swiper)`
+  /* background-color: red; */
   width: 100vw;
   height: 100vh;
-  border: 1px solid red;
-  background: url(${img1}) no-repeat;
+`;
+const SwiperCard = styled.div<{ imgUrl: string }>`
+  width: 100vw;
+  height: 100vh;
+  background: url(${(props) => props.imgUrl}) no-repeat;
   background-size: cover;
 `;
-
 const Banner = () => {
-  return <Container></Container>;
+  return (
+    <>
+      <SwiperContainer
+        spaceBetween={0}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <SwiperCard imgUrl={img1}>나는신이다</SwiperCard>
+        </SwiperSlide>
+        <SwiperSlide>
+          <SwiperCard imgUrl={img2}></SwiperCard>
+        </SwiperSlide>
+        <SwiperSlide>
+          <SwiperCard imgUrl={img3}></SwiperCard>
+        </SwiperSlide>
+      </SwiperContainer>
+    </>
+  );
 };
-
 export default Banner;
