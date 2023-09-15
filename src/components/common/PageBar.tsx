215f5c374c3d4bf092300dca114ae001200a60f3
jk6722 <jk6722@hanyang.ac.kr>
2023-09-15 11:34:57 +0900

    chore(#76): 불필요한 import 제거

--- src/components/common/PageBar.tsx
+++ src/components/common/PageBar.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect, useRef } from 'react';
+import { useCallback } from 'react';
 import styled from 'styled-components';
 
 import ArrowLeft from '/assets/icon/arrow-left.svg';
