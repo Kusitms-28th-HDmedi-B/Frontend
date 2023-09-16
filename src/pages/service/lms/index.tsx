0962b2593b3d162b688724fd164d1cf1454ac06f
j2noo <wlsdndml213@naver.com>
2023-09-17 01:29:04 +0900

    feat(#98) : 메인배너 뺴고 완료

--- src/pages/service/lms/index.tsx
+++ src/pages/service/lms/index.tsx
@@ -8,9 +8,12 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Uppinfo from './Uppinfo';
 
+import systemImg from '/assets/image/lms/lms-system.png';
+import lastBanner from '/assets/image/lms/lms-last-banner.png';
+import { Link } from 'react-router-dom';
+
 const Container = styled.div`
   margin: auto;
-  margin-bottom: 100px;
   white-space: break-spaces;
 `;
 
@@ -97,6 +100,77 @@ const UppBelt = styled.div`
   text-align: center;
   line-height: 200px;
 `;
+const LmsSystem = styled.span`
+  background: url(${systemImg}) no-repeat;
+  background-size: cover;
+  width: 100vw;
+  height: 80vw;
+  display: inline-block;
+`;
+const LastBanner = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #d9d9d9;
+  width: 100vw;
+  height: 80vh;
+  span {
+    background: url(${lastBanner}) no-repeat;
+    background-size: cover;
+    width: 600px;
+    height: 400px;
+    display: inline-block;
+    margin: 100px;
+    border-radius: 10px;
+  }
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    position: relative;
+    margin-right: 120px;
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+  }
+`;
+const LastText = styled.div`
+  white-space: break-spaces;
+  display: inline-block;
+  font-size: 48px;
+  line-height: 70px;
+  font-weight: 700;
+
+  text-align: center;
+
+  display: inline-block;
+`;
+const FloatBtn = styled.div`
+  display: inline-block;
+  width: 200px;
+  height: 60px;
+  border-radius: 30px;
+  background-color: #6a6a6a;
+  color: white;
+  text-align: center;
+  line-height: 60px;
+  cursor: pointer;
+  margin-top: 50px;
+
+  /* 둥둥 떠있게 하기 */
+  animation: float 3s ease-in-out infinite;
+  @keyframes float {
+    0%,
+    100% {
+      transform: translateY(0);
+    }
+    50% {
+      transform: translateY(-20px);
+    }
+  }
+`;
 const Lms = () => {
   const setCurrHeader = useSetRecoilState(currHeaderAtom);
   useEffect(() => setCurrHeader('transparent'));
@@ -133,7 +207,21 @@ const Lms = () => {
         <div></div>
       </Text2>
       <UppBelt>UPP는 어떤 서비스인가요?</UppBelt>
-      <Uppinfo/>
+      <Uppinfo />
+      <LmsSystem></LmsSystem>
+      <LastBanner>
+        <span></span>
+        <div>
+          <LastText>{'교육과 관리를 한 번에,\nUPP와 함께 하세요!'}</LastText>
+          <Link
+            to={
+              'https://upp.notion.site/UPP-LMS-CMS-5cdf38ccd5164fb2b085cf70295d8b4b'
+            }
+          >
+            <FloatBtn>서비스 바로가기</FloatBtn>
+          </Link>
+        </div>
+      </LastBanner>
     </Container>
   );
 };
