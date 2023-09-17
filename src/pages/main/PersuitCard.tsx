9704d82f275fc593c2dbdc17c7c675df07951829
j2noo <wlsdndml213@naver.com>
2023-09-17 14:02:59 +0900

    feat(#69) : blurCard 반응형

--- src/pages/main/PersuitCard.tsx
+++ src/pages/main/PersuitCard.tsx
@@ -3,8 +3,8 @@ import { persuitCardProps } from './bannerData';
 
 const Container = styled.div`
   position: relative;
-  height: 350px;
-  width: 350px;
+  height: 300px;
+  width: 300px;
   border-radius: 20px;
   padding: 30px;
   font-weight: 600;
@@ -19,6 +19,13 @@ const Container = styled.div`
     top: 80px;
     height: auto;
   }
+
+  @media screen and (max-width: 500px) {
+    height: 300px;
+    width: 300px;
+    padding: 20px;
+    margin-bottom: 20px;
+  }
 `;
 const CardImg = styled.span<{ $imgUrl: string }>`
   position: absolute; //부모인 CardContainer에 겹치게 하기 위한 설정
@@ -30,6 +37,9 @@ const CardImg = styled.span<{ $imgUrl: string }>`
   background: url(${(props) => props.$imgUrl}) left top no-repeat;
   background-size: cover;
   display: inline-block;
+  @media screen and (max-width: 500px) {
+    filter: blur(20px);
+  }
 `;
 const Title = styled.div<{ $titleColor: string }>`
   position: absolute; //부모인 CardContainer에 겹치게 하기 위한 설정
@@ -39,18 +49,31 @@ const Title = styled.div<{ $titleColor: string }>`
 
   font-size: 30px;
   margin-bottom: 30px;
+  @media screen and (max-width: 500px) {
+    font-size: 25px;
+    margin-bottom: 20px;
+  }
 `;
 const StyledUl = styled.ul`
   position: absolute; //부모인 CardContainer에 겹치게 하기 위한 설정
   top: 100px;
   height: 0px;
   overflow: hidden;
+
+  @media screen and (max-width: 500px) {
+    top: 80px;
+    height: auto;
+  }
   li {
     white-space: break-spaces;
     margin: 10px 0;
     font-size: 14px;
     font-weight: 600;
     line-height: 26px;
+    @media screen and (max-width: 500px) {
+      font-size: 13px;
+      line-height: 20px;
+    }
   }
 `;
 const PersuitCard: React.FC<persuitCardProps> = ({
