0a0d771a10fd522d8a17d0d6e0739f189cb31181
j2noo <wlsdndml213@naver.com>
2023-09-17 11:11:43 +0900

    feat(#104) : 헤더 반응형 완료

--- src/components/layout/HeaderMobile.tsx
+++ src/components/layout/HeaderMobile.tsx
@@ -14,23 +14,6 @@ const MobileContainer = styled.div`
     display: inline;
     width: 100vw;
     height: auto;
-    /* background-color: red; */
-  }
-  ul {
-    /* width: 0px; */
-    height: 0px;
-    overflow: hidden;
-    transition: height 0.5s ease-in-out;
-
-    font-size: 18px;
-    font-weight: 600;
-    li {
-      margin: 15px;
-      padding: 5px 0;
-    }
-  }
-  ul.toggleOn {
-    height: 500px;
   }
 `;
 const MobileHeader = styled.div`
@@ -57,24 +40,68 @@ const HamburgerBar = styled.span`
     cursor: pointer;
   }
 `;
-const HamburgerDepthContainer = styled.ul`
+const HamburgerUl = styled.ul`
+  @media screen and (max-width: 500px) {
+    padding: 0 20px;
+    height: 0px;
+    overflow: hidden;
+    transition: height 0.3s ease-in-out;
+
+    font-size: 18px;
+    font-weight: 600;
+
+    &.toggleOn {
+      height: 100vh;
+    }
+    > li {
+      &.clicked {
+        text-decoration: underline;
+      }
+    }
+  }
+`;
+const HamburgerDepthUl = styled.ul<{ $countDepth: number }>`
   @media screen and (max-width: 500px) {
     display: inline-block;
 
-    width: 30px;
-    height: 23px;
+    width: 300px;
+    height: 0px;
+    margin: 15px 0;
+    overflow: hidden;
+    transition: height 0.3s ease-in-out;
 
     cursor: pointer;
+
+    &.clicked {
+      /* height: 50px; */
+      height: ${(props) => props.$countDepth * 41}px;
+    }
+
     li {
-      background-color: red;
-      width: 30px;
+      color: #616161;
+      font-size: 14px;
+      font-weight: 600;
+      width: 500px;
       height: 23px;
+      margin: 10px 0;
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
     }
   }
 `;
 
 const HeaderMobile = () => {
   const [toggle, setToggle] = useState(false);
+  const [isClicked, setisClicked] = useState([
+    true,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
 
   return (
     <MobileContainer>
@@ -83,24 +110,40 @@ const HeaderMobile = () => {
           <Logo></Logo>
         </Link>
         <HamburgerBar onClick={() => setToggle((curr) => !curr)}></HamburgerBar>
-        <ul className={toggle ? 'toggleOn' : 'toggleOff'}>
-          {CategoryData.map((category) => {
-            return (
-              <>
-                <li>{category.title}</li>
-
-                <HamburgerDepthContainer>
-                  {category.depth.map((each, index) => (
-                    <li key={index}>
-                      <Link to={each.url}>{each.name}</Link>
-                    </li>
-                  ))}
-                </HamburgerDepthContainer>
-              </>
-            );
-          })}
-        </ul>
       </MobileHeader>
+
+      <HamburgerUl className={toggle ? 'toggleOn' : ''}>
+        {CategoryData.map((category, index) => {
+          return (
+            <>
+              <li
+                className={isClicked[index] ? 'clicked' : ''}
+                onClick={() =>
+                  setisClicked((curr) => {
+                    const currState = curr[index];
+                    let newClicked = [...curr];
+                    newClicked[index] = !currState;
+                    return newClicked;
+                  })
+                }
+              >
+                {category.title}
+              </li>
+
+              <HamburgerDepthUl
+                className={isClicked[index] ? 'clicked' : ''}
+                $countDepth={category.depth.length}
+              >
+                {category.depth.map((each, index) => (
+                  <li onClick={() => setToggle(false)} key={index}>
+                    <Link to={each.url}>{each.name} </Link>
+                  </li>
+                ))}
+              </HamburgerDepthUl>
+            </>
+          );
+        })}
+      </HamburgerUl>
     </MobileContainer>
   );
 };
